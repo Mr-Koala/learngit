e25722d1645d0dfe1a17d8be0b3643aca34cb55c
Mr-Koala <koala.mr@foxmail.com>
2014-11-24 22:53:53 +0800

    add two source file

---
+++ class.c
@@ -0,0 +1,18 @@
+#include "iostream"
+using namespace std
+class Shape
+{
+public:
+	Shape(arguments);
+	~Shape();
+
+	/* data */
+};
+class Point:public Shape
+{
+public:
+	Point(arguments);
+	~Point();
+
+	/* data */
+};
---
+++ main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int int main(int argc, char const *argv[])
+{
+	printf("Stdin is ");
+	return 0;
+}
